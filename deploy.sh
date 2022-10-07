336d111103d04732e696bc493fe8dafa28a2304a
Marko Ma <yma004@drury.edu>
2022-10-07 11:25:14 +0800

    update gitignore and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PROJECT_ID=$(gcloud projects list --filter="name:'My First Project'" --format='value(PROJECT_ID)')
 
-terraform plan -var="project_id=${PROJECT_ID}"
+terraform apply -var="project_id=${PROJECT_ID}"
